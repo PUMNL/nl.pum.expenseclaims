b5a75562cd0f6e9b07df57a778e3c2c5947191c8
ruudgithub <ruudgithub@users.noreply.github.com>
2018-04-09 18:08:44 +0200

    Fixed #1422: A draft claim should be removable, add files

---
+++ api/v3/ClaimLog/Delete.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * ClaimLog.Delete API specification (optional)
+ * This is used for documentation and validation.
+ *
+ * @param array $spec description of fields supported by this API call
+ * @return void
+ * @see http://wiki.civicrm.org/confluence/display/CRM/API+Architecture+Standards
+ */
+function _civicrm_api3_claim_log_delete_spec(&$spec) {
+  $spec['id'] = array(
+    'name' => 'id',
+    'title' => 'id',
+    'type' => CRM_Utils_Type::T_INT,
+    'api.required' => 1
+  );
+}
+
+/**
+ * ClaimLog.Delete API
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */
+function civicrm_api3_claim_log_delete($params) {
+  if (array_key_exists('id', $params)) {
+    return civicrm_api3_create_success(CRM_Expenseclaims_BAO_ClaimLog::deleteWithActivityId($params['id']), $params, 'ClaimLog', 'Delete');
+  } else {
+    throw new API_Exception('Id is a mandatory param when deleting a claim line', 'mandatory_id_missing', 0020);
+  }
+}
+
