f42812a552e97f7b27f82238536b61e19f6bc760
ruudgithub <ruudgithub@users.noreply.github.com>
2020-06-22 10:09:19 +0200

    Fixed #4745: Add ProjectType, expert sector and client country to export

--- CRM/Expenseclaims/Page/BatchExport.php
+++ CRM/Expenseclaims/Page/BatchExport.php
@@ -11,62 +11,80 @@ class CRM_Expenseclaims_Page_BatchExport extends CRM_Core_Page {
 
     $claimsql = "
  SELECT   cact.id  AS claim_id
- ,        ci.{$config->getClaimLinkCustomField('column_name')} AS claim_link
- ,        ci.{$config->getClaimTotalAmountCustomField('column_name')} AS claim_total_amount
- ,        ci.{$config->getClaimDescriptionCustomField('column_name')} AS claim_description
- ,        ci.{$config->getClaimStatusCustomField('column_name')}     AS claim_status_id
- ,        ci.{$config->getClaimTypeCustomField('column_name')} AS claim_type_id
- ,        cac.contact_id AS claim_submitted_by
- ,        adata.shortname_14 AS shortname
- ,        c.display_name AS display_name
- ,        adr.street_address AS street_address
- ,        adr.postal_code    AS postal_code
- ,        adr.city           AS city
- ,        adr.country_id     AS country_id
+ ,        ci.{$config->getClaimLinkCustomField('column_name')}          AS claim_link
+ ,        ci.{$config->getClaimTotalAmountCustomField('column_name')}   AS claim_total_amount
+ ,        ci.{$config->getClaimDescriptionCustomField('column_name')}   AS claim_description
+ ,        ci.{$config->getClaimStatusCustomField('column_name')}        AS claim_status_id
+ ,        ci.{$config->getClaimTypeCustomField('column_name')}          AS claim_type_id
+ ,        cac.contact_id                                                AS claim_submitted_by
+ ,        adata.shortname_14                                            AS shortname
+ ,        c.display_name                                                AS display_name
+ ,        adr.street_address                                            AS street_address
+ ,        adr.postal_code                                               AS postal_code
+ ,        adr.city                                                      AS city
+ ,        adr.country_id                                                AS country_id
 
- ,        {$config->getBankInformationCustomFields()['IBAN_nummer']} AS iban_number
- ,        {$config->getBankInformationCustomFields()['BIC_Swiftcode']} AS bic_swiftcode
- ,        {$config->getBankInformationCustomFields()['Accountholder_name']} AS accountholder_name
- ,        {$config->getBankInformationCustomFields()['Accountholder_address']} AS accountholder_address
- ,        {$config->getBankInformationCustomFields()['Accountholder_postal_code']} AS accountholder_postal_code
- ,        {$config->getBankInformationCustomFields()['Accountholder_city']} AS accountholder_city
- ,        {$config->getBankInformationCustomFields()['Accountholder_country']}  AS accountholder_country
- ,        {$config->getBankInformationCustomFields()['Foreign_Bank_Account']} AS foreign_bank_account
- ,        {$config->getBankInformationCustomFields()['Bank_Account_Number']} AS bank_account_number
- ,        cact.activity_date_time AS claim_submitted_date
- ,        csov.label AS claim_status
+ ,        {$config->getBankInformationCustomFields()['IBAN_nummer']}                AS iban_number
+ ,        {$config->getBankInformationCustomFields()['BIC_Swiftcode']}              AS bic_swiftcode
+ ,        {$config->getBankInformationCustomFields()['Accountholder_name']}         AS accountholder_name
+ ,        {$config->getBankInformationCustomFields()['Accountholder_address']}      AS accountholder_address
+ ,        {$config->getBankInformationCustomFields()['Accountholder_postal_code']}  AS accountholder_postal_code
+ ,        {$config->getBankInformationCustomFields()['Accountholder_city']}         AS accountholder_city
+ ,        {$config->getBankInformationCustomFields()['Accountholder_country']}      AS accountholder_country
+ ,        {$config->getBankInformationCustomFields()['Foreign_Bank_Account']}       AS foreign_bank_account
+ ,        {$config->getBankInformationCustomFields()['Bank_Account_Number']}        AS bank_account_number
+ ,        cact.activity_date_time                   AS claim_submitted_date
+ ,        csov.label                                AS claim_status
 
- ,        ctov.label AS claim_type
- ,        ctov.grouping AS gl_account_number
- ,        ctov.name     AS fa_default_donor
- ,        line.expense_date AS expense_date
- ,        line.expense_type AS expense_type
- ,        ltov.grouping AS pum_account_number
- ,        curr.name AS currency
- ,        line.currency_amount AS currency_amount
- ,        line.euro_amount AS euro_amount
- ,         line.exchange_rate AS exchange_rate
- ,         line.description AS description
- ,         line.distance_km AS distance_km
- ,         line.cost_center AS cost_center
- ,         pum_case.case_sequence AS case_sequence
- ,         pum_case.case_type AS case_type
- ,         pum_case.case_country AS case_country
- FROM   pum_claim_batch cb
- JOIN   pum_claim_batch_entity   cbe  ON  (cbe.batch_id = cb.id and cbe.entity_table='civicrm_activity')
- LEFT   JOIN   civicrm_activity  cact  ON  (cact.id = cbe.entity_id)
- LEFT   JOIN   civicrm_activity_contact cac ON   (cbe.entity_id = cac.activity_id and cac.record_type_id=2)
- LEFT   JOIN   civicrm_contact          c   ON   (cac.contact_id = c.id)
- LEFT   JOIN   civicrm_address          adr ON   (adr.contact_id = c.id and adr.is_primary=1)
- LEFT   JOIN   civicrm_value_bank_information_11 bank ON (bank.entity_id = c.id)
- LEFT   JOIN   civicrm_value_additional_data_4 adata ON (adata.entity_id = c.id)
- LEFT   JOIN   pum_claim_line line ON line.activity_id = cact.id
- LEFT   JOIN   {$config->getClaimInformationCustomGroup('table_name')} ci on (ci.entity_id = cbe.entity_id)
- LEFT   JOIN   civicrm_currency  curr ON (curr.id = line.currency_id)
- LEFT   JOIN   civicrm_case_pum  pum_case ON (pum_case.entity_id =  ci.{$config->getClaimLinkCustomField('column_name')})
- LEFT   JOIN   civicrm_option_value csov ON ci.{$config->getClaimStatusCustomField('column_name')} = csov.value AND csov.option_group_id = {$config->getClaimStatusOptionGroup('id')}
- LEFT   JOIN   civicrm_option_value   ctov ON ci.{$config->getClaimTypeCustomField('column_name')} = ctov.value AND ctov.option_group_id = {$config->getClaimTypeOptionGroup('id')}
- LEFT   JOIN   civicrm_option_value   ltov ON (line.expense_type = ltov.value collate utf8_general_ci AND ltov.option_group_id = {$config->getClaimLineTypeOptionGroup('id')})
+ ,        ctov.label                                AS claim_type
+ ,        ctov.grouping                             AS gl_account_number
+ ,        ctov.name                                 AS fa_default_donor
+ ,        line.expense_date                         AS expense_date
+ ,        line.expense_type                         AS expense_type
+ ,        ltov.grouping                             AS pum_account_number
+ ,        curr.name                                 AS currency
+ ,        line.currency_amount                      AS currency_amount
+ ,        line.euro_amount                          AS euro_amount
+ ,        line.exchange_rate                        AS exchange_rate
+ ,        line.description                          AS description
+ ,        line.distance_km                          AS distance_km
+ ,        line.cost_center                          AS cost_center
+ ,        pum_case.case_sequence                    AS case_sequence
+ ,        pum_case.case_type                        AS case_type
+ ,        pum_case.case_country                     AS case_country
+ ,        IFNULL(ccny.name, pccl.display_name)      AS client_country
+ ,        seg.label                                 AS expert_sector
+ ,        cap.label                                 AS case_name
+ FROM         pum_claim_batch                   cb
+ JOIN         pum_claim_batch_entity            cbe       ON (cbe.batch_id = cb.id and cbe.entity_table='civicrm_activity')
+ LEFT   JOIN  civicrm_activity                  cact      ON (cact.id = cbe.entity_id)
+ LEFT   JOIN  civicrm_activity_contact          cac       ON (cbe.entity_id = cac.activity_id and cac.record_type_id=2)
+ LEFT   JOIN  civicrm_contact                   c         ON (cac.contact_id = c.id)
+ LEFT   JOIN  civicrm_address                   adr       ON (adr.contact_id = c.id and adr.is_primary=1)
+ LEFT   JOIN  civicrm_value_bank_information_11 bank      ON (bank.entity_id = c.id)
+ LEFT   JOIN  civicrm_value_additional_data_4   adata     ON (adata.entity_id = c.id)
+ LEFT   JOIN  pum_claim_line                    line      ON line.activity_id = cact.id
+ LEFT   JOIN  {$config->getClaimInformationCustomGroup('table_name')} ci on (ci.entity_id = cbe.entity_id)
+ LEFT   JOIN  civicrm_currency                  curr      ON (curr.id = line.currency_id)
+ LEFT   JOIN  civicrm_case_pum                  pum_case  ON (pum_case.entity_id =  ci.{$config->getClaimLinkCustomField('column_name')})
+ LEFT   JOIN  civicrm_option_value              csov      ON ci.{$config->getClaimStatusCustomField('column_name')} = csov.value AND csov.option_group_id = {$config->getClaimStatusOptionGroup('id')}
+ LEFT   JOIN  civicrm_option_value              ctov      ON ci.{$config->getClaimTypeCustomField('column_name')} = ctov.value AND ctov.option_group_id = {$config->getClaimTypeOptionGroup('id')}
+ LEFT   JOIN  civicrm_option_value              ltov      ON (line.expense_type = ltov.value collate utf8_general_ci AND ltov.option_group_id = {$config->getClaimLineTypeOptionGroup('id')})
+ LEFT   JOIN  civicrm_case                      cas       ON cas.id = ci.pum_projectnumber_referencenumbe_387
+ LEFT   JOIN  civicrm_option_value              cap       ON cap.option_group_id = (SELECT id FROM civicrm_option_group WHERE name = 'case_type') AND cap.value = cas.case_type_id
+ /* To display country in export */
+  LEFT JOIN civicrm_case ctp ON ctp.id = ci.pum_projectnumber_referencenumbe_387
+  LEFT JOIN civicrm_case_contact pccc ON pccc.case_id = ci.pum_projectnumber_referencenumbe_387
+  LEFT JOIN civicrm_contact pccl ON (
+    CASE pccl.contact_sub_type WHEN CONCAT(char(1),'Country',char(1)) THEN pccl.id = pccc.contact_id END
+  )
+  LEFT JOIN civicrm_address padr ON padr.contact_id = pccc.contact_id AND padr.is_primary = 1
+  LEFT JOIN civicrm_country ccny ON ccny.id = padr.country_id
+ /* End display country in export */
+ /* To display expert sector in export */
+  LEFT JOIN civicrm_contact_segment               cs        ON cs.contact_id = c.id AND cs.is_active = 1 AND cs.is_main = 1
+  LEFT JOIN civicrm_segment                       seg       ON seg.id = cs.segment_id AND seg.is_active = 1
+ /* End display expert sector in export */
  WHERE  cb.id = %1";
 
     return $claimsql;
@@ -128,7 +146,11 @@ function run() {
       'exchange_rate',
       'description',
       'distance_km',
-      'cost_center'
+      'cost_center',
+      'Projecttype',
+      'Sector',
+      'Artikel',        //Strange column name for client country is due to financial system that require these columns
+      'Omschrijving'    //Strange column name for client country is due to financial system that require these columns
     );
 
     $buffer = implode(';',array_map("CRM_Expenseclaims_Utils::csvField",$heading))."\n";
@@ -180,7 +202,12 @@ function run() {
        $dao->exchange_rate,
        $dao->description,
        $dao->distance_km,
-       $dao->cost_center
+       $dao->cost_center,
+
+       $dao->case_name,
+       $dao->expert_sector,
+       $dao->client_country,
+       $dao->client_country                             //2 times client country is correct, it due to financial system that require these columns
      );
 
      $buffer .= implode(';',array_map("CRM_Expenseclaims_Utils::csvField",$line))."\n";
